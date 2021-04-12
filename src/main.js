875cebf6d82baa2977f090adb3ba711f067ad958
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2021-04-12 09:21:09 +0700

    Boiler Plate & Depedencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
